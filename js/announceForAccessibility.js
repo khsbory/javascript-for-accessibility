05ec84e7efe7b0313ce9ec6538c5bc52e617e9e2
jongseop-park <59268378+jongseop-park@users.noreply.github.com>
2021-11-15 11:02:02 +0900

    announceForAccessibility.js 수정 V1.1

--- js/announceForAccessibility.js
+++ js/announceForAccessibility.js
@@ -10,21 +10,27 @@ document.getElementsByTagName("head")[0].appendChild(oScript);
 
 /**
  * div 안 p 태그에 메시지 삽입 후 body 끝에 추가하며 2초 후 삭제하는 함수
- * div 의 name 은 announceForAccessibility 를 사용함 (해당 태그를 삭제하기 위함)
+ * div 의 name 은 div_announceForAccessibility 를 사용함 (해당 태그를 삭제하기 위함)
  */
 function announceForAccessibility(message) {
   var html = '' +
-    '<div aria-live="polite" name="announceForAccessibility" style="border: 0; padding: 0; margin: 0; ' +
+    '<div aria-live="polite" name="div_announceForAccessibility" style="border: 0; padding: 0; margin: 0; ' +
     'position: absolute !important;' + 'height: 1px; width: 1px; overflow: hidden; clip: rect(1px 1px 1px 1px); ' +
-    'clip: rect(1px, 1px, 1px, 1px);' + 'clip-path: inset(50%); white-space: nowrap;"><p>'+ message +'</p></div>';
+    'clip: rect(1px, 1px, 1px, 1px);' + 'clip-path: inset(50%); white-space: nowrap;">' +
+    '<p name="p_announceForAccessibility"></p></div>';
 
-  $("body").append(html);
-  setTimeout(removeAnnounceForAccessibility,2000);
+  $("body").append(html); // body 끝에 div_announceForAccessibility 추가
+
+  setTimeout(function () { // 0.3초 후 p 태그에 message 추가
+    $("[name='p_announceForAccessibility']").text(message);
+  }, 300);
+
+  setTimeout(removeAnnounceForAccessibility, 2000); // 2초 후 div_announceForAccessibility 삭제
 }
 
 /**
- * div 의 name 이 announceForAccessibility 인 태그를 하위까지 삭제
+ * div 의 name 이 div_announceForAccessibility 인 태그를 하위까지 삭제
  */
 function removeAnnounceForAccessibility() {
-  $("[name='announceForAccessibility']").remove();
+  $("[name='div_announceForAccessibility']").remove();
 }
