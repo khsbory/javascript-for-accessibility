4ab434793fdc490f3570d8433667fb0968440edc
jongseop-park <59268378+jongseop-park@users.noreply.github.com>
2021-11-15 12:59:33 +0900

    announceForAccessibility.js V1.2

--- js/announceForAccessibility.js
+++ js/announceForAccessibility.js
@@ -5,7 +5,7 @@
 var oScript = document.createElement("script");
 oScript.type = "text/javascript";
 oScript.charset = "utf-8";
-oScript.src = "http://code.jquery.com/jquery-3.6.0.min.js";
+oScript.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 document.getElementsByTagName("head")[0].appendChild(oScript);
 
 /**
