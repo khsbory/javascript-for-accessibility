e952e7e7213a8cfb58f4ea2eca49fe51be95d53f
jongseop-park <59268378+jongseop-park@users.noreply.github.com>
2021-11-19 17:25:36 +0900

    announceForAccessibility.js V1.4

--- js/announceForAccessibility.js
+++ js/announceForAccessibility.js
@@ -1,20 +1,22 @@
 /**
- * Jquery 라이브러리 최신 버전 호출
- * 이미 최신 버전이 존재할 경우 호출하지 않음
+ * jQuery 라이브러리 버전이 1.7.0 미만이거나 없을 경우 jQuery 최신 버전 적용
  */
-var headScript = document.getElementsByTagName("head")[0].getElementsByTagName("script");
-for (var i = 0, check = false; i < headScript.length; i++){
-  if ("https://code.jquery.com/jquery-latest.min.js" === headScript[i].src){
-    check = true;
-    break;
+try {
+  var jScript = document.createElement("script");
+  jScript.type = "text/javascript";
+  jScript.charset = "utf-8";
+  jScript.src = "https://code.jquery.com/jquery-latest.min.js";
+  
+  var jQuery_version = jQuery.fn.jquery.split(".").map(Number); // 페이지에 적용된 jQuery 버전을 가져옴
+  var jQuery_minimum_version = 17; // jQuery 최소 버전 1.7.0
+  
+  jQuery_version = jQuery_version[0] * 10 + jQuery_version[1];
+  if (!(jQuery_version >= jQuery_minimum_version)){
+	  console.log("dd")
+	  document.getElementsByTagName("head")[0].appendChild(jScript);
   }
-}
-if (!check){
-  var oScript = document.createElement("script");
-  oScript.type = "text/javascript";
-  oScript.charset = "utf-8";
-  oScript.src = "https://code.jquery.com/jquery-latest.min.js";
-  document.getElementsByTagName("head")[0].appendChild(oScript);
+} catch { // jQuery가 없어 에러가 발생한 경우 최신 버전을 적용함
+	document.getElementsByTagName("head")[0].appendChild(jScript);
 }
 
 window.addEventListener('load', function() {
